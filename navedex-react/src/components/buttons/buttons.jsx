7067fa5fb400d88597f33f1a88aa45a8b39580e9
isaitavares <50214000+isaitavares@users.noreply.github.com>
2020-09-29 21:10:14 -0300

    feat: minor CSS added for testing

---
+++ navedex-react/src/components/buttons/buttons.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+  const Button = styled.button`
+    height: 40px;
+    width: 384px;
+    color: black;
+`;
+export default Button
---
+++ navedex-react/src/components/containers/contentContainer.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const ContentContainer = styled.div`
+    height:100%;
+    width:100%;
+`;
+
+export default ContentContainer
