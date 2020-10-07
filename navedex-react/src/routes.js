ef92dd7789c240192c04f215b132b57b104e05a1
isaitavares <50214000+isaitavares@users.noreply.github.com>
2020-10-07 19:32:33 -0300

    Feat: new "add" and "edit" users route added

--- navedex-react/src/routes.js
+++ navedex-react/src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route }  from 'react-router-dom';
-import login from './routes/Login/login';
-import home from './routes/Home/home';
-
+import Login from './routes/Login';
+import Home from './routes/Home';
+import RouteHandler from './routes/RouterHandler'
+import AdicionarNaver from './routes/AdicionarNaver'
+import EditarNaver from './routes/EditarNaver'
 function Routes(){
     return(
         <BrowserRouter>
-            <Route path='/login' exact component={login} />
-            <Route path='/home' exact component={home} />
+            <Route path='/login' exact component={Login} />
+            <Route path='/home' exact component={Home} />
+            <Route path='/adicionarnaver' exact component={AdicionarNaver} />
+            <Route path='/editarnaver' exact component={EditarNaver} />
+            <Route path='/' exact component={RouteHandler} />
         </BrowserRouter>            
     );
 }
